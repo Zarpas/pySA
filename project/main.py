9f7716339c11134126a445db42ad6a60bc04521a
srisankethu <sanketh.uppalapati@research.iiit.ac.in>
2016-04-10 23:14:17 +0530

    Tidy up main.py

--- project/main.py
+++ project/main.py
@@ -2,18 +2,22 @@ from solve import get_sfd,get_bmd
 from takeinput import set_input
 from plot import plotter
 
-values = set_input()
-loads = []
-moments = []
-positions = []
-span = values.get('span')
-position = 0
 
-while (position<=span):
-    loads.append(get_sfd(values, position))
-    moments.append(get_bmd(values, position))
-    positions.append(position)
-    position+=float(span)/100
+def main():
+    values = set_input()
+    loads = []
+    moments = []
+    positions = []
+    span = values.get('span')
+    position = 0
 
-plotter(loads, positions, 'Shear force')
-plotter(moments, positions, 'Bending Moment')
+    while (position<=span):
+        loads.append(get_sfd(values, position))
+        moments.append(get_bmd(values, position))
+        positions.append(position)
+        position+=float(span)/100
+
+    plotter(loads, positions, 'Shear force')
+    plotter(moments, positions, 'Bending Moment')
+
+main()
